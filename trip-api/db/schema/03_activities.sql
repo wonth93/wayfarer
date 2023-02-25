baf0e1d1c5e5ca0e5fb65e287097f62296d4d3fb
Vanessa Power <vanessa.l.power@gmail.com>
2023-02-25 13:32:22 -0800

    fixed bug in acitivites table

--- trip-api/db/schema/03_activities.sql
+++ trip-api/db/schema/03_activities.sql
@@ -8,8 +8,8 @@ CREATE TABLE activities (
   trip_id INTEGER REFERENCES trips(id) ON DELETE CASCADE,
   activity_name VARCHAR(255) NOT NULL,
   activity_address VARCHAR(255) NOT NULL,
-  lat DECIMAL(8,6)
-  long DECIMAL(9,6)
+  lat DECIMAL(8,6),
+  long DECIMAL(9,6),
   activity_cost INTEGER NOT NULL DEFAULT 0,
   activity_date DATE NOT NULL,
   activity_time TIME NOT NULL,
