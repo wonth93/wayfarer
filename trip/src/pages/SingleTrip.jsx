f8b4ae2588f1959e61b5fcc34b8a87153e03f3aa
Vanessa Power <vanessa.l.power@gmail.com>
2023-02-28 16:50:23 -0800

    Move single trip components into separate folder

--- trip/src/pages/SingleTrip.jsx
+++ trip/src/pages/SingleTrip.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import TripInfoContainer from "../components/TripInfoContainer"
+import TripInfoContainer from "../components/Single_Trip/TripInfoContainer"
 import { useParams } from 'react-router-dom';
 
 const SingleTrip = () => {
